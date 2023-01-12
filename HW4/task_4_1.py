654f999615b34774979365a1cbd7998aec7e9103
Alexey <aleksei_gudovich@mail.ru>
2023-01-12 21:23:33 +0300

    Task_4_1

---
+++ HW4/task_4_1.py
@@ -0,0 +1,21 @@
+# Вычислить число c заданной точностью d
+# in
+# Enter a real number: 9
+# Enter the required accuracy '0.0001': 0.000001
+
+# out
+# 9.000000
+
+# in
+# Enter a real number: 8.98785
+# Enter the required accuracy '0.0001': 0.001
+
+# out
+# 8.988
+
+from decimal import Decimal
+
+p = input('Введите число: ')
+a = Decimal(p)
+a = a.quantize(Decimal("1.0000"))
+print(a)
